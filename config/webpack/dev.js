52b09a6ddcbdfb5e995de0c3ae26941933b5f36b
Liang Jin <Liang.Jin@capitalone.com>
2016-10-25 21:35:53 -0400

    init commit

---
+++ config/webpack/dev.js
@@ -0,0 +1,32 @@
+const baseConfig = require('./base'),
+	webpack = require('webpack'),
+	_ = require('lodash'),
+	path = require('path');
+
+const devConfig = baseConfig;
+
+const port = process.env.PORT || 9090;
+
+
+devConfig.entry = [
+	`webpack-hot-middleware/client?path=http://localhost:${port}/__webpack_hmr`,
+	'./src/index'
+];
+
+devConfig.devtools = "source-map";
+
+devConfig.debug = true;
+
+devConfig.output.publicPath = `http://localhost:${port}/build/`;
+
+devConfig.plugins = _.concat(devConfig.plugins, [
+	new webpack.HotModuleReplacementPlugin(),
+	new webpack.NoErrorsPlugin(),
+	new webpack.DefinePlugin({
+		'process.env.NODE_ENV': JSON.stringify('development')
+	})
+]);
+
+devConfig.target = 'electron-renderer';
+
+module.exports = devConfig;
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const root = <h1>I love minecraft  me too!</h1>;
+
+ReactDOM.render(
+    root,
+    document.getElementById("bootstrapContainer")
+);
