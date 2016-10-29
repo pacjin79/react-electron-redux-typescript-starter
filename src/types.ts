819db0b458f55fb08b351c3c6c5b89f87e0b4927
Liang Jin <Liang.Jin@capitalone.com>
2016-10-28 23:36:53 -0400

    fixed bugs

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,6 @@
 import NotificationSystem = require("react-notification-system");
 import Notification = NotificationSystem.Notification;
+import * as React from 'react';
 
 interface IPageProperties {
     notifications: Array<Notification>;
